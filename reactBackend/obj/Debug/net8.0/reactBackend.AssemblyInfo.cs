00f998ea90b4f4e7668dc5cddbfa261b5121065b
JosueCast <126531292+JosueCast@users.noreply.github.com>
2025-02-11 22:15:06 -0600

    Guia VI: Introduccion a Asp.net

--- reactBackend/obj/Debug/net8.0/reactBackend.AssemblyInfo.cs
+++ reactBackend/obj/Debug/net8.0/reactBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reactBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428f5d2ea4d17c3273e9823759e0867c8efe679b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52cf94a6e222105ca31c683fcb8d20853e9114f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("reactBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reactBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- temporal/obj/Debug/net8.0/temporal.AssemblyInfo.cs
+++ temporal/obj/Debug/net8.0/temporal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("temporal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428f5d2ea4d17c3273e9823759e0867c8efe679b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52cf94a6e222105ca31c683fcb8d20853e9114f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("temporal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("temporal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
